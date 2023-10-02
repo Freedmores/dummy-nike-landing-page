903b077f861aa72680fca8e3ac22657c34c93b74
freedmores <freedmores@gmail.com>
2023-10-02 17:22:38 +0200

    Fixed an error on customer reviews

--- src/sections/index.js
+++ src/sections/index.js
@@ -2,7 +2,7 @@ import Hero from "./Hero";
 import PopularProducts from "./PopularProducts";
 import Footer from "./Footer";
 import Services from "./Services";
-import CustomerReviews from "./customerReviews";
+import CustomerReviews from "./CustomerReviews";
 import SpecialOffer from "./SpecialOffer";
 import Subscribe from "./Subscribe";
 import SuperQuality from "./SuperQuality";
