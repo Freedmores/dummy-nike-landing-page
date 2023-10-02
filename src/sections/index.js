4f492faa29181df6fa7fd3a48442079494c472e6
freedmores <freedmores@gmail.com>
2023-10-02 17:01:20 +0200

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import Footer from "./Footer";
+import Services from "./Services";
+import CustomerReviews from "./customerReviews";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+
+export {
+  Hero,
+  PopularProducts,
+  Footer,
+  Services,
+  CustomerReviews,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+};
